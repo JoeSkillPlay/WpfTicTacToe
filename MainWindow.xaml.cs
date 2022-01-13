ca03a0c1ce88ea0499c90767159672597a565da2
JoeSkillPlay <dans.puduls17@gmail.com>
2022-01-13 19:27:53 +0200

    Game prototype ready

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfTicTacToe
 {
@@ -47,6 +37,7 @@ namespace WpfTicTacToe
                 tb.Text = turn;
 
                 SwitchTurn();
+                CheckForWin();
 
             }
 
@@ -60,13 +51,67 @@ namespace WpfTicTacToe
             if (turn == "X")
             {
                 turn = "O";
-                MessageBox.Show("Next turn - O");
             }
+
             else
             {
                 turn = "X";
-                MessageBox.Show("Next turn - X");
+
             }
+        }
+
+
+        private void CheckForWin()
+        {
+            var winColor = Brushes.Orange;
+
+            if (t1.Text != "        " && t1.Text == t2.Text && t2.Text == t3.Text)
+            {
+
+                t1.Foreground = t2.Foreground = t3.Foreground = winColor;
+                GameOver();
+
+            }
+            else if(t4.Text != "          " && t4.Text == t5.Text && t5.Text == t6.Text)
+            {
+                t4.Foreground = t5.Foreground = t6.Foreground = winColor;
+                GameOver();
+            }
+            else if (t7.Text != "         " && t7.Text == t8.Text && t8.Text == t9.Text)
+            {
+                t7.Foreground = t8.Foreground = t9.Foreground = winColor;
+                GameOver();
+            }
+            else if (t1.Text != "         " && t1.Text == t4.Text && t4.Text == t7.Text)
+            {
+                t1.Foreground = t4.Foreground = t7.Foreground = winColor;
+                GameOver();
+            }
+            else if (t2.Text != "         " && t2.Text == t5.Text && t5.Text == t8.Text)
+            {
+                t2.Foreground = t5.Foreground = t8.Foreground = winColor;
+                GameOver();
+            }
+            else if (t3.Text != "         " && t3.Text == t5.Text && t5.Text == t9.Text)
+            {
+                t3.Foreground = t5.Foreground = t9.Foreground = winColor;
+                GameOver();
+            }
+            else if (t1.Text != "         " && t1.Text == t5.Text && t5.Text == t9.Text)
+            {
+                t1.Foreground = t5.Foreground = t9.Foreground = winColor;
+                GameOver();
+            }
+            else if (t3.Text != "         " && t3.Text == t5.Text && t5.Text == t7.Text)
+            {
+                t3.Foreground = t5.Foreground = t7.Foreground = winColor;
+                GameOver();
+            }
+        }
+
+        private void GameOver()
+        {
+            MessageBox.Show($"Game over, {turn} wins!");
         }
 
     }
